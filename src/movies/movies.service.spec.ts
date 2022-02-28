c3d3dab06c275594771e0d45838a217ad515050b
kjy5657 <45618920+kjy5657@users.noreply.github.com>
2022-02-28 19:43:26 +0900

    unit testing(jest)

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,8 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
   let service: MoviesService;
+  const createMovie = () => {
+    service.create({
+      title: 'Test Movie',
+      genres: ['test'],
+      year: 2000,
+    });
+    service.create({
+      title: 'Test Movie2',
+      genres: ['test2'],
+      year: 2020,
+    });
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -10,9 +23,87 @@ describe('MoviesService', () => {
     }).compile();
 
     service = module.get<MoviesService>(MoviesService);
+    createMovie();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be 4', () => {
+    expect(2 + 2).toEqual(4);
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+
+    it('should throw 404 error', () => {
+      const id = 999;
+      try {
+        service.getOne(id);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual(`movie with id ${id} not found`);
+      }
+    });
+  });
+
+  describe('deleteOne', () => {
+    it('delete a movie', () => {
+      const beforeDelete = service.getAll();
+      service.deleteOne(1);
+      const afterDelete = service.getAll();
+
+      expect(afterDelete.length).toEqual(beforeDelete.length - 1);
+      expect(afterDelete.find((movie) => movie.id === 1)).toBeUndefined();
+    });
+    it('should throw NotFoundException', () => {
+      const id = 999;
+      try {
+        service.deleteOne(id);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual(`movie with id ${id} not found`);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+      createMovie();
+      const afterCreate = service.getAll().length;
+      expect(beforeCreate + 2).toEqual(afterCreate);
+    });
+  });
+
+  describe('update', () => {
+    const id = 1;
+    it('should update a movie', () => {
+      service.update(id, { title: 'updated test' });
+      const movie = service.getOne(id);
+      expect(movie.title).toEqual('updated test');
+    });
+
+    it('should throw NotFoundException', () => {
+      const id = 999;
+      try {
+        service.update(id, { title: 'updated test' });
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual(`movie with id ${id} not found`);
+      }
+    });
   });
 });
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
@@ -12,11 +12,19 @@ export class MoviesService {
   }
 
   getOne(id: number): Movie {
-    return this.movies.find((movie) => movie.id === id);
+    const movie = this.movies.find((movie) => movie.id === id);
+    if (!movie) {
+      throw new NotFoundException(`movie with id ${id} not found`);
+    }
+    return movie;
   }
 
   deleteOne(id: number): boolean {
-    this.movies.filter((movie) => movie.id !== id);
+    const movie = this.getOne(id);
+    if (!movie) {
+      throw new NotFoundException(`movie with id ${id} not found`);
+    }
+    this.movies = this.movies.filter((movie) => movie.id !== id);
     return true;
   }
 
@@ -30,6 +38,9 @@ export class MoviesService {
   //이름에 상관없이, arg의 순서에 따라 결정
   update(id: number, updatedData: UpdateMovieDto) {
     const movie = this.getOne(id);
+    if (!movie) {
+      throw new NotFoundException(`movie with id ${id} not found`);
+    }
     this.deleteOne(id);
     this.movies.push({ ...movie, ...updatedData });
   }
